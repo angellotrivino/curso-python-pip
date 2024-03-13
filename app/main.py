283ca4da36f19b15fba174c5a0d53a5ea8019fc0
Angello Triviño Umaña <angello.trivinio@gmail.com>
2024-03-13 14:36:51 -0500

    web-service with html FastAPI

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
     
-    data = read_csv.read_csv("data.csv")
-    
     '''
-    
     Primer reto graficar la poblacion de un pais en diferentes
     periodos de tiempo
     '''
+
+    data = read_csv.read_csv("data.csv")
     country = input('Type Country => ')
     result = utils.population_by_country(data, country)
     name_country = country
@@ -26,13 +26,20 @@ def run():
     Segundo reto: Graficar esta columna contra los paises
     '''
     if len(data) > 0:
-        data = list(filter(lambda x: x['Continent'] == 'South America', data))
+        
+        ##Usando lambda
+        #data = list(filter(lambda x: x['Continent'] == 'South America', data))
+        #country_list = list(map(lambda x: x['Country'], data))
+        #population_list = list(map(lambda x: x['World Population Percentage'], data))
+        #new_data = {keys: values for keys, values in zip(country_list, population_list)}
+        #labels = new_data.keys()
+        #values = new_data.values()
 
-        country_list = list(map(lambda x: x['Country'], data))
-        population_list = list(map(lambda x: x['World Population Percentage'], data))
-        new_data = {keys: values for keys, values in zip(country_list, population_list)}
-        labels = new_data.keys()
-        values = new_data.values()
+        #Usando pandas
+        df = pd.read_csv('data.csv')
+        df = df[df['Continent'] == 'Africa']
+        labels = df['Country'].values
+        values = df['World Population Percentage'].values
 
         charts.generate_pie_chart(labels, values)
 
